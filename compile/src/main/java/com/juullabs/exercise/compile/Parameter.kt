ae780bcac1bdb15d4845b1f1d02bc2a1d3dcdb96
Cedrick Cooke <development@cedrickc.net>
2020-01-28 15:54:59 -0800

    Add support for lists as an argument type

--- compile/src/main/java/com/juullabs/exercise/compile/Parameter.kt
+++ compile/src/main/java/com/juullabs/exercise/compile/Parameter.kt
@@ -1,8 +1,10 @@
 package com.juullabs.exercise.compile
 
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.asTypeName
+import com.sun.tools.javac.code.Attribute
 import javax.lang.model.element.AnnotationMirror
 import javax.lang.model.type.TypeMirror
 
@@ -18,19 +20,32 @@ internal class Parameter(
 
     companion object {
 
-        private fun adjustTypeName(type: TypeMirror): TypeName {
-            val rawTypeName = type.asTypeName()
-            return when ((rawTypeName as? ClassName)?.canonicalName) {
+        private fun adjustTypeName(type: TypeMirror): TypeName =
+            adjustTypeName(type.asTypeName())
+
+        private fun adjustTypeName(raw: TypeName): TypeName =
+            when ((raw as? ClassName)?.canonicalName) {
+                List::class.java.canonicalName -> listTypeName
                 String::class.java.canonicalName -> stringTypeName
-                else -> rawTypeName
+                else -> raw
             }
-        }
 
         fun fromAnnotation(annotation: AnnotationMirror): Parameter {
             val values = annotation.elementValues.mapKeys { (k, _) -> k.simpleName.toString() }
+            val rawNonNullTypeName = adjustTypeName(values["type"]?.value as TypeMirror)
+            val arguments = checkNotNull(values["typeArguments"]).value as List<Attribute.Class>
+            val nonNullTypeName = if (arguments.isEmpty()) {
+                rawNonNullTypeName
+            } else {
+                val parameters = arguments.map { adjustTypeName(it.classType.asTypeName()) }
+                with(ParameterizedTypeName.Companion) {
+                    check(rawNonNullTypeName is ClassName)
+                    rawNonNullTypeName.parameterizedBy(parameters)
+                }
+            }
             return Parameter(
-                name = checkNotNull(values["name"]).value as String,
-                nonNullTypeName = adjustTypeName(checkNotNull(values["type"]).value as TypeMirror),
+                name = values["name"]?.value as String,
+                nonNullTypeName = nonNullTypeName,
                 optional = values["optional"]?.value == true
             )
         }
