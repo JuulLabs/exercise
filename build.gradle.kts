b5cb92b3f073ff6e4c89e968a9b68472f59b6e1f
Cedrick Cooke <development@cedrickc.net>
2021-07-08 15:59:09 -0700

    Update for Kotlin 1.5.10 and KSP beta01

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.4.30" apply false
-    id("com.android.application") version "4.0.0" apply false
-    id("com.android.library") version "4.0.0" apply false
-    id("org.jmailen.kotlinter") version "3.2.0" apply false
-    id("binary-compatibility-validator") version "0.2.3"
-    id("org.jetbrains.dokka") version "1.4.30" apply false
-    id("com.vanniktech.maven.publish") version "0.13.0" apply false
+    kotlin("jvm") version "1.5.10" apply false
+    id("com.android.application") version "4.2.0" apply false
+    id("com.android.library") version "4.2.0" apply false
+    id("org.jmailen.kotlinter") version "3.4.5" apply false
+    id("binary-compatibility-validator") version "0.6.0"
+    id("org.jetbrains.dokka") version "1.5.0" apply false
+    id("com.vanniktech.maven.publish") version "0.17.0" apply false
     id("net.mbonnin.one.eight") version "0.2"
 }
 
@@ -21,15 +21,6 @@ subprojects {
     repositories {
         mavenCentral()
         google()
-        jcenter {
-            content {
-                // https://youtrack.jetbrains.com/issue/IDEA-261387
-                includeModule("org.jetbrains.trove4j", "trove4j")
-
-                // https://github.com/Kotlin/kotlinx.html/issues/173
-                includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
-            }
-        }
     }
 
     tasks.withType<Test>().configureEach {
--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -2,12 +2,7 @@ plugins {
     id("com.android.library")
     kotlin("android")
     id("kotlin-parcelize")
-    id("org.jmailen.kotlinter")
-    jacoco
-    id("com.vanniktech.maven.publish")
 }
-
-apply(from = rootProject.file("gradle/jacoco.gradle.kts"))
 
 android {
     compileSdkVersion(AndroidSdk.Compile)
