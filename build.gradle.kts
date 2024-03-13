7ace33e90991a36055d76c2f6a6761e24450b23d
Travis Wyatt <travis@juul.com>
2024-03-13 09:12:02 -0700

    Simplify `binary-compatibility-validator` setup (#280)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlinter) apply false
-    alias(libs.plugins.validator)
+    alias(libs.plugins.api)
     alias(libs.plugins.dokka)
     alias(libs.plugins.maven.publish) apply false
 }
